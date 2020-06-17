0b4b69439cd8ff1836850f2529a85ec3fbd6fbf0
Joabe Santana <joabesantana.c@gmail.com>
2020-06-16 21:33:38 -0300

    Adição do ProfileContext

--- App.js
+++ App.js
@@ -7,34 +7,52 @@ import { NavigationContainer } from '@react-navigation/native';
 import Camera from './screen/Camera';
 import ProfileToEdit from './screen/ProfileToEdit';
 import ChooseIcon from './screen/ChooseIcon';
-
+import { ProfileContext } from './ProfileContext'
 const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='Abas'
-          component={Abas}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name='Camera'
-          component={Camera}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name='ProfileToEdit'
-          component={ProfileToEdit}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name='ChooseIcon'
-          component={ChooseIcon}
-          options={{ headerShown: true }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.changeProfile = this.changeProfile.bind(this);
+    this.state = {
+      user: 'José',
+      changeProfile: this.changeProfile,
+    };
+
+  }
+
+  changeProfile(newProfile) {
+    this.setState({ user: newProfile.name });
+  }
+  render() {
+    return (
+      <ProfileContext.Provider value={this.state} >
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name='Abas'
+              component={Abas}
+              options={{ headerShown: true }}
+            />
+            <Stack.Screen
+              name='Camera'
+              component={Camera}
+              options={{ headerShown: true }}
+            />
+            <Stack.Screen
+              name='ProfileToEdit'
+              component={ProfileToEdit}
+              options={{ headerShown: true }}
+            />
+            <Stack.Screen
+              name='ChooseIcon'
+              component={ChooseIcon}
+              options={{ headerShown: true }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ProfileContext.Provider>
+    );
+  }
 }
